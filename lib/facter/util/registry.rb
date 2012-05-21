8f0e5581104921206e47e97ddcfb59da1f846b2e
Josh Cooper <josh@puppetlabs.com>
2012-05-21 13:55:08 -0700

    (#12864) Close registry key
    Previously, we were not closing the registry key (until the process
    exits). This commit just uses the block form of `open` then
    automatically closes the key.

--- lib/facter/util/registry.rb
+++ lib/facter/util/registry.rb
@@ -2,8 +2,7 @@ module Facter::Util::Registry
   class << self
     def hklm_read(key, value)
       require 'win32/registry'
-      reg = Win32::Registry::HKEY_LOCAL_MACHINE.open(key)
-      reg[value]
+      Win32::Registry::HKEY_LOCAL_MACHINE.open(key) {|reg| reg[value] }
     end
   end
 end
