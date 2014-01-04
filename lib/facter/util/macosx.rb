28f5fe27babc9a062b2b2c69a91fbeb9adc5bebb
Josh Cooper <josh@puppetlabs.com>
2014-01-03 17:21:11 -0800

    (FACT-94) Unvendor CFPropertyList
    This commit unvendors CFPropertyList, and instead relies the runtime
    dependency expressed in project_data.yaml.

--- lib/facter/util/macosx.rb
+++ lib/facter/util/macosx.rb
@@ -7,8 +7,8 @@
 
 module Facter::Util::Macosx
   require 'thread'
-  require 'facter/util/cfpropertylist'
-  require 'facter/util/resolution'
+  require 'CFPropertyList'
+p  require 'facter/util/resolution'
 
   Plist_Xml_Doctype  = '<!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">'
 
@@ -26,13 +26,13 @@ module Facter::Util::Macosx
       xml.gsub!( bad_xml_doctype, Plist_Xml_Doctype )
       Facter.debug("Had to fix plist with incorrect DOCTYPE declaration")
     end
-    plist = Facter::Util::CFPropertyList::List.new
+    plist = CFPropertyList::List.new
     begin
       plist.load_str(xml)
-    rescue => e
-      fail("A plist file could not be properly read by Facter::Util::CFPropertyList: #{e.inspect}")
+    rescue CFPlistError => e
+      fail("A plist file could not be properly read by CFPropertyList: #{e.inspect}")
     end
-    Facter::Util::CFPropertyList.native_types(plist.value)
+    CFPropertyList.native_types(plist.value)
   end
 
   # Return an xml result, modified as we need it.
