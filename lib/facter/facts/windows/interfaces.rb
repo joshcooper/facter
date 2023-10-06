3e2c01e189335f9ea9eebaef8473cea88a07b990
Josh Cooper <joshcooper@users.noreply.github.com>
2023-10-06 14:39:10 -0700

    (FACT-3428) Style/SafeNavigation

--- lib/facter/facts/windows/interfaces.rb
+++ lib/facter/facts/windows/interfaces.rb
@@ -9,7 +9,7 @@ module Facts
       def call_the_resolver
         fact_value = Facter::Resolvers::Windows::Networking.resolve(:interfaces)
 
-        Facter::ResolvedFact.new(FACT_NAME, fact_value ? fact_value.keys.join(',') : nil, :legacy)
+        Facter::ResolvedFact.new(FACT_NAME, fact_value ? fact_value.keys.join(',') : nil, :legacy) # rubocop:disable Style/SafeNavigation
       end
     end
   end
